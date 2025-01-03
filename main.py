ea3fe4a3b88bd6f8eb21a95f1c9f59e54169021c
Ali Pilehvar Meibody <ali.pilehvarmeibody@studenti.polito.it>
2025-01-03 20:21:38 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,64 @@
+#" IN GOD WE TRUST, ALL OTHERS MUST BRING DATA"
+#                                               -W. Edwards Deming
+#------------------------------------------------------------------------------
+# Copyright 2023 The Gamlab Authors. All Rights Reserved.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#     http://www.apache.org/licenses/LICENSE-2.0
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#------------------------------------------------------------------------------
+''' 
+The Scientific experimental simulation library 
+-------------------------------------------------------------------------------
+Graphen & Advanced Material Laboratory 
+
+it aimes to provide new scientist to use data,simlation, prepared data 
+and Artificial intelligence models.
+
+See http://gamlab.aut.ac.ir for complete documentation.
+'''
+__doc__='''
+
+@author: Ali Pilehvar Meibody (Alipilehvar1999@gmail.com)
+
+                                         888                    888
+ .d8888b    .d88b.     88888b.d88b.      888         .d88b.     888
+d88P"      d88""88b    888 "888 "88b     888        d88""88b    88888PP
+888  8888  888  888    888  888  888     888        888  888    888  888
+Y88b.  88  Y88..88PP.  888  888  888     888......  Y88..88PP.  888  888
+ "Y8888P8   "Y88P8888  888  888  888     888888888   "Y88P8888  88888888  
+
+
+@Director of Gamlab: Professor M. Naderi (Mnaderi@aut.ac.ir)    
+
+@Graphene Advanced Material Laboratory: https://www.GamLab.Aut.ac.ir
+
+
+'''
+
+import numpy as np
+import random
+import statictic
+import scipy
+import pandas as pd
+
+
+__version__ = "0.0"
+
+
+
+def get_info():
+    print('Besie constants there are two type of functions: 1-first functions 2-converter functions')
+    print('----------------------')
+    print('All contsants is listed below: \n     pi,ti,si,ci,ki,l,m,n,t,q,r,s \n     s,g,h,t,xc')
+    print('----------------------')
+    print('All functions is listed below:\n [Generate_building] , [ generating_host] ,[generating_soup] \n')
+    print('----------------------')
+    print('Converters is listed below: \n [celisius to farenhite] , [farenhite to celisius]')
+    print('All contsants is listed below: \n     pi,ti,si,ci,ki,l,m,n,t,q,r,s \n     s,g,h,t,xc')
+    
