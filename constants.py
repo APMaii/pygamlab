cf58fdf44119aa1f06c783d546707afb19da0152
Ali Pilehvar Meibody <ali.pilehvarmeibody@studenti.polito.it>
2025-01-03 20:22:35 +0100

    Create constants.py

---
+++ constants.py
@@ -0,0 +1,152 @@
+#" IN GOD WE TRUST, ALL OTHERS MUST BRING DATA"
+#                                               -W. Edwards Deming
+#------------------------------------------------------------------------------
+# Copyright 2023 The Gamlab Authors. All Rights Reserved.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#     http://www.apache.org/licenses/LICENSE-2.0
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#------------------------------------------------------------------------------
+''' 
+The Scientific experimental simulation library 
+-------------------------------------------------------------------------------
+Graphen & Advanced Material Laboratory 
+
+it aimes to provide new scientist to use data,simlation, prepared data 
+and Artificial intelligence models.
+
+See http://gamlab.aut.ac.ir for complete documentation.
+'''
+__doc__='''
+
+@author: Ali Pilehvar Meibody (Alipilehvar1999@gmail.com)
+
+                                         888                    888
+ .d8888b    .d88b.     88888b.d88b.      888         .d88b.     888
+d88P"      d88""88b    888 "888 "88b     888        d88""88b    88888PP
+888  8888  888  888    888  888  888     888        888  888    888  888
+Y88b.  88  Y88..88PP.  888  888  888     888......  Y88..88PP.  888  888
+ "Y8888P8   "Y88P8888  888  888  888     888888888   "Y88P8888  88888888  
+
+
+@Director of Gamlab: Professor M. Naderi (Mnaderi@aut.ac.ir)    
+
+@Graphene Advanced Material Laboratory: https://www.GamLab.Aut.ac.ir
+
+
+@Co-authors: 
+'''
+
+
+
+
+#Constant-------------------------------------------
+pi=3.1415926536
+e=2.7182818284
+c=299792458
+N_a=6.022*10**23
+R_J=8.3143
+R_Cal=1.987
+R_LA=0.08205
+Pi=3.14
+g=9.81
+Tau=Pi*2
+e=7.1828
+pi = 3.14159
+e = 2.71828
+g = 9.81                                          # Gravitational constant
+h =  6.634 * 10**-34                              # constant pelank
+k , b = 1.38 * 10 ** -23                          # J/mjck constant of boltzman       
+tau = 6.28
+R =0.08206           #unit = ((L*atm) / (mol*k)) # Global constant of gases
+pi=3.14
+ta=6.28
+e=2.718
+zeta=1.202
+#Golden ratio
+phi=1.618
+N_a = 6.022*10**23              #molecules/mol (Avogadro's Number)
+k = 1.381*10**(-23)             #J/K (Boltzmann's Constant)
+alpha = 5.67*10**(-8)           #W/m^2/K^4 (Stefan-Boltzmann Constant)
+R = 8.314                       #J/mol/K (Gas costant)
+Visible = range(380, 750, 10)   #Visible Wavelength
+h = 6.626*10**(-34)             #J.s (Planck's Constant)
+c = 2.998*10**8                 #m/s (speed of light)
+pi=3.14
+R=8.314
+Na=6.022*(10**23)
+e=2.7182818284590452353602874713527
+Boltzmann_Constant=1.380649*(10**(-23))
+Conductivity_PPY=105
+Conductivity_pT=33.7
+Conductivity_P3HT=2.4
+Conductivity_PLN=10
+
+Kw = 1*10**-14  # Autoionization constant for water at 25°C
+Fe_Tm_Alpha=910
+'دمای ذوب آهن در فاز آلفا است'
+
+Fe_Tm_Gama=1495
+'دمای ذوب آهن در فاز گاما است'
+
+Fe_Tm_Delta=1539
+'دمای ذوب آهن در فاز دلتا است'
+
+Fe_Density=7.87
+'چگالی آهن و فولاد'
+
+max_C_inSteel=2.11
+'بیشترین درصد کربن در فولاد'
+'در واقع بیشتر از این مقدار کربن وارد فولاد شود به چدن تبدیل میشود'
+
+Eutectic_T=1148
+'دمای واکنش یوتکتیک در دیاگرام آهن کربن'
+
+Eutectic_Percent=4.3
+'درصد کربن در فولاد که در آن واکنش یوتکتیک رخ میدهد'
+
+Eutectoid_T=727
+'دمای واکنش یوتکتوئید'
+
+Eutectoid_persent=0.76
+'درصد کربن در واکنش یوتکتوئید'
+
+
+Pi=3.14159265358979323846264338327950288419716939937510
+C=299792458
+#m/s; speed of light in vacuum
+R= 8.314510
+#J/(mol*K); molar gas constant
+g0=9.81 #m/s2
+
+density_of_Fe=7.87
+'Here, density of Fe is reported in g per cubic cm'
+
+density_of_Al=2.7
+'Here, density of Al is reported in g per cubic cm'
+
+density_of_Cu=8.96
+'Here, density of Cu is reported in g per cubic cm'
+
+melting_point_of_Fe=1538
+'Here, melting point of Fe is reported in kelvin'
+
+melting_point_of_Al=660
+'Here, melting point of Al is reported in kelvin'
+
+melting_point_of_Cu=1085
+'Here, melting point of Cu is reported in kelvin'
+
+thermal_conductivity_coefficient_of_Fe=221
+'Here, thermal conductivity coefficient of Fe is reported in W/mK'
+
+thermal_conductivity_coefficient_of_Al=237
+'Here, thermal conductivity coefficient of Al is reported in W/mK'
+
+thermal_conductivity_coefficient_of_Cu=385
+'Here, thermal conductivity coefficient of Cu is reported in W/mK'
