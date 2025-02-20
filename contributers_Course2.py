a43a2721a90fae8a1dc48ce57b9e9d1c9979998f
Ali Pilehvar Meibody <ali.pilehvarmeibody@studenti.polito.it>
2025-02-20 21:48:31 +0100

    Update contributers_Course2.py

--- contributers_Course2.py
+++ contributers_Course2.py
@@ -1,8 +1,7 @@
 
-
 '''
 
-
+cours2
 
 
 maryam haj heydari
@@ -37,6 +36,39 @@ alireza peymani 1 function
 '''
 
 
+'''
+course3_contributers.py-----------------------
+contributers
+
+A2-Mohammad-Shafiee
+A2_Sam_Beigy
+A2_Najmeh_ Azizizadeh
+A2_Naghmeh_Gholamalizadeh2
+A2_Mohamadali_Rabiei
+A2_MINA_MOHAMMADI
+A2_MARZIEH_ALIDADI
+A2_Mahdi_Nematollahi
+A2_Fatemeh-Babaei
+A2_Elnaz_Askari
+A2_Behnaz_Hadi
+A2_ARSHAWN_REZVANIPOUR
+A2_Amin_Abedi
+A1-Mohammad-Shafiee
+A1_Zahra_Tamimi
+A1_pariya_isvandi.py
+A1_Najmeh_ Azizizadeh.py
+A1_Naghmeh_Gholamalizadeh.py
+A1_Marziyeh_Hatami-2.py
+A1_MARZIEH_ALIDADI-2
+A1_Mahdi_Nematollahi
+A1_Fatemeh Babaei
+A1_Behnaz_Hadi-2
+A1_ARSHAWN_REZVANIPOUR
+A1_Amin_Abedi
+A1_Ali_Moftakharzadeh
+
+
+'''
 
 
 
