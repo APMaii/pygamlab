bb7add0f684fc9c502116bb827f938614ea043ae
Ali Pilehvar Meibody <ali.pilehvarmeibody@studenti.polito.it>
2025-02-20 21:03:29 +0100

    Update and rename Course2.py to contributers_Course2.py

---
+++ contributers_Course2.py
@@ -0,0 +1,45 @@
+
+
+'''
+
+
+
+
+maryam haj heydari
+mojtaba bakhshi
+shahin esfandiari
+masooome
+narges tehrani
+atena jalilzadeh
+erfan jalilzadeh
+narges bagherie
+zahra afshar
+niloofar kazemkhanloo
+nazanin nasiri
+
+shahrokh akbari
+hamed taghipoor
+parinaz hajiali
+hanie salehi
+zahra moradi
+dental_material
+mohammadmahdi yektafallah
+
+pavane seyedpour
+zahra moradi
+nasim heidarian
+
+
+Babak yadegari 1 function
+alireza peymani 1 function
+
+
+'''
+
+
+
+
+
+
+
+
