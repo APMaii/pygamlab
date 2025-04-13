c4b50887cedd02f3bdb3d45ccac73f38164bb2b4
Ali Pilehvar Meibody <ali.pilehvarmeibody@studenti.polito.it>
2025-04-13 16:17:00 +0330

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -43,3 +43,13 @@ Y88b.  88  Y88..88PP.  888  888  888     888......  Y88..88PP.  888  888
 '''
 
 
+
+__version__ = "0.1.2"
+
+
+
+__all__ = ["Constants", "Convertors", "Functions", "Data_Analysis"]
+
+
+
+
