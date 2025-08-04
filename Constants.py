d8da48f1071ccea231383c2cf910ee4fe248276d
Ali Pilehvar Meibody <ali.pilehvarmeibody@studenti.polito.it>
2025-08-04 19:53:19 +0330

    Update Constants.py

--- Constants.py
+++ Constants.py
@@ -1,9 +1,9 @@
 '''
 Constants.py ==> 
 
-
 This module contains scientific constants intended for use throughout the package.
 '''
+
 
 #" IN GOD WE TRUST, ALL OTHERS MUST BRING DATA"
 #                                               -W. Edwards Deming
