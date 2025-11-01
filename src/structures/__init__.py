d6cd851e90a3098c74d8c0c5b80482da0d38a3dc
Ali Pilehvar Meibody <ali.pilehvarmeibody@studenti.polito.it>
2025-11-01 18:41:38 +0100

    Add files via upload

---
+++ src/structures/__init__.py
@@ -0,0 +1,29 @@
+"""
+Structures subpackage for pygamlab:
+Contains atomic primitives, nanostructure architectures, I/O utilities, and visualization.
+"""
+
+from .Primatom import *
+from .Generators import *
+from .GAM_architectures import *
+from .gamvis import *
+from .io import *
+
+__all__ = ['GAM_Atom', 'GAM_Bond', 'GAM_Molecule',
+           'Nano_ZeroD_Builder', 'Nano_OneD_Builder', 'Nano_TwoD_Builder', 'AdvancedAlloys',
+           'Graphene', 'Silicene', 'Phosphorene', 'Nanoparticle_Generator', 'Nanotube_Generator',
+           'Molecular_Visualizer', 'GAMVisualizer',
+           'read_structure', 'export', 'gam_to_ase', 'ase_to_gam', 'gam_to_pymatgen','pymatgen_to_gam','detect_format']
+
+
+
+
+#__all__ = ['Primatom','Generator','GAM_architectures',
+#           'gamvis',
+#           'io']
+
+
+
+
+
+
