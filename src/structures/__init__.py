19a0ec9a6c80db1129b6875d388e6f487885e20e
APMaii <ali.pilehvarmeibody@studenti.polito.it>
2025-12-07 22:29:24 +0100

    change in init of structures

--- src/structures/__init__.py
+++ src/structures/__init__.py
@@ -2,7 +2,6 @@
 Structures subpackage for pygamlab:
 Contains atomic primitives, nanostructure architectures, I/O utilities, and visualization.
 """
-
 from .Primatom import *
 from .Generators import *
 from .GAM_architectures import *
@@ -14,8 +13,6 @@ __all__ = ['GAM_Atom', 'GAM_Bond', 'GAM_Molecule',
            'Graphene', 'Silicene', 'Phosphorene', 'Nanoparticle_Generator', 'Nanotube_Generator',
            'Molecular_Visualizer', 'GAMVisualizer',
            'read_structure', 'export', 'gam_to_ase', 'ase_to_gam', 'gam_to_pymatgen','pymatgen_to_gam','detect_format']
-
-
 
 
 #__all__ = ['Primatom','Generator','GAM_architectures',
